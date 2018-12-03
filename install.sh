2bfef1cb62799f4213a0de02bf53970b48763aa3
UrsSchmidt <ursbjoernschmidt@googlemail.com>
2018-12-03 10:15:39 +0100

    Moved source files into src/

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-path='/usr/share/gtksourceview-3.0/language-specs/'
-langs=('antlr' 'bnf' 'cuesheet' 'ebnf' 'fb2' 'fb2query' 'jasmin' 'jimple' 'pl0' 'rgbasm')
+path='/usr/share/gtksourceview-3.0/language-specs'
 
 if [ -d "$path" ]; then
-    for lang in "${langs[@]}"; do
-        cp "$lang.lang" "$path"
+    for f in 'src/'*.lang; do
+        cp "$f" "$path/$(basename $f)"
     done
 else
     echo "Error: $path is not a directory"
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 path='/usr/share/gtksourceview-3.0/language-specs'
-langs=('antlr' 'bnf' 'cuesheet' 'ebnf' 'fb2' 'fb2query' 'jasmin' 'jimple' 'pl0' 'rgbasm')
 
 if [ -d "$path" ]; then
-    for lang in "${langs[@]}"; do
-        if [ -f "$path/$lang.lang" ]; then
-            rm "$path/$lang.lang"
-        fi
+    for f in 'src/'*.lang; do
+        rm "$path/$(basename $f)"
     done
 else
     echo "Error: $path is not a directory"
